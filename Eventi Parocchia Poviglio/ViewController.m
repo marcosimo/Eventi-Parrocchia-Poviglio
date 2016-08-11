70a62062306bcdd6383214ff80c102dabc052863
marco <marco@Mac-Pro-di-marco.local>
2016-08-11 10:55:09 +0200

    Boh

--- Eventi Parocchia Poviglio/ViewController.m
+++ Eventi Parocchia Poviglio/ViewController.m
@@ -14,14 +14,66 @@
 
 @implementation ViewController
 
+UIDeviceOrientation currentOrientation;
+UIWebView* webview;
+int dy = 20;
+
 - (void)viewDidLoad {
     [super viewDidLoad];
-    // Do any additional setup after loading the view, typically from a nib.
+    NSString *public_server = @"www.vitaparrpov.altervista.org";
+    NSString *local_server = @"marcosim.homepc.it:8082";
+    NSString *server = local_server;
+    
+    NSDictionary *dictionary = [[NSDictionary alloc] initWithObjectsAndKeys:
+                                @"Mozilla/5.0 (iPhone; like Mac OS X) AppleWebKit/600.1.4 (KHTML, like Gecko) Mobile vitaparrpovmobile/2.0", @"UserAgent", nil];
+    [[NSUserDefaults standardUserDefaults] registerDefaults:dictionary];
+    
+    NSString *startpage = [[@"http://" stringByAppendingString:server] stringByAppendingString:@"/app/mobileEvents/"];
+    
+    NSURL *url = [NSURL URLWithString:startpage];
+    
+    webview=[[UIWebView alloc] init];
+    
+    [webview setDelegate:self];
+    
+    webview.frame = CGRectMake(0, dy, self.view.frame.size.width, self.view.frame.size.height-dy);
+    
+    NSURLRequest *nsrequest=[NSURLRequest requestWithURL:url];
+    [webview loadRequest:nsrequest];
+    webview.scalesPageToFit = false; // non forza lo zoom al 100%
+    [self.view addSubview:webview];
+}
+
+- (void)webViewDidFinishLoad:(UIWebView *)webView{
+    //NSString *theTitle=[webView stringByEvaluatingJavaScriptFromString:@"document.title"];
+    //[webView loadHTMLString:theTitle baseURL:[NSURL URLWithString:@""]];
+}
+
+- (void)viewWillLayoutSubviews {
+    [super viewWillLayoutSubviews];
+    if(self.view.frame.size.width > self.view.frame.size.height) {
+        webview.frame = CGRectMake(0, 0, self.view.frame.size.width, self.view.frame.size.height);
+    } else {
+        webview.frame = CGRectMake(0, dy, self.view.frame.size.width, self.view.frame.size.height-dy);
+    }
 }
 
 - (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
-    // Dispose of any resources that can be recreated.
+    
+}
+
+- (BOOL)webView:(UIWebView *)webView shouldStartLoadWithRequest:(NSURLRequest *)request navigationType:(UIWebViewNavigationType)navigationType {
+    
+    if([[[request URL] absoluteString] containsString:@"__vpextlnk__"]) {
+        NSString *urlstr = [[[request URL] absoluteString] stringByReplacingOccurrencesOfString:@"__vpextlnk__" withString:@""];
+        NSURL *url = [NSURL URLWithString:urlstr];
+        
+        [[UIApplication sharedApplication] openURL:url];
+        
+        return NO;
+    }
+    return YES;
 }
 
 @end
